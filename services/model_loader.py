857fe6704b8d27cd55101a34e83a6608ade2dea1
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-05-12 12:35:12 +0530

    Update model_loader.py

--- services/model_loader.py
+++ services/model_loader.py
@@ -6,4 +6,4 @@ load_dotenv()
 
 def load_llm():
     groq_api_key = os.getenv("GROQ_API_KEY")
-    return ChatGroq(temperature=0, groq_api_key=groq_api_key, model_name="deepseek-r1-distill-llama-70b")
+    return ChatGroq(temperature=0, groq_api_key=groq_api_key, model_name="llama-3.3-70b-versatile")
